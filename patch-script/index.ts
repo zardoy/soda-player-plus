7df9a859837f4676caa519da4cf09033005e41e4
Vitaly Turovsky <vital2580@icloud.com>
2021-03-01 15:20:42 +0300

    cleanup even if patch failed

--- patch-script/index.ts
+++ patch-script/index.ts
@@ -37,54 +37,54 @@ export const sodaPlayerBasicConfig: Omit<AppConfig, "patchContents"> = {
 
 export const patchSodaPlayer = async (customPatchDirectory?: string) => {
     const tmpDirForDownloadingPatch = os.tmpdir();
-    await patchElectronApp({
-        ...sodaPlayerBasicConfig,
-        async patchContents({ contentsDir }) {
-            let patchDir: string;
-            if (customPatchDirectory) {
-                patchDir = customPatchDirectory;
-            } else {
-                const downloadPatchUrl = "https://github.com/zardoy/soda-player-plus/archive/main.zip";
+    try {
+        await patchElectronApp({
+            ...sodaPlayerBasicConfig,
+            async patchContents({ contentsDir }) {
+                let patchDir: string;
+                if (customPatchDirectory) {
+                    patchDir = customPatchDirectory;
+                } else {
+                    const downloadPatchUrl = "https://github.com/zardoy/soda-player-plus/archive/main.zip";
 
-                await download(downloadPatchUrl, tmpDirForDownloadingPatch, {
-                    filename: "patch-archive"
-                });
-                const patchArchive = path.join(tmpDirForDownloadingPatch, "patch-archive");
-                const adm = new AdmZip(patchArchive);
-                await new Promise(resolve =>
-                    adm.extractAllToAsync(path.join(tmpDirForDownloadingPatch, "patch"), true, resolve));
-                rimraf.sync(patchArchive);
-                patchDir = path.resolve(tmpDirForDownloadingPatch, "patch/soda-player-plus-main/patch");
+                    await download(downloadPatchUrl, tmpDirForDownloadingPatch, {
+                        filename: "patch-archive"
+                    });
+                    const patchArchive = path.join(tmpDirForDownloadingPatch, "patch-archive");
+                    const adm = new AdmZip(patchArchive);
+                    await new Promise(resolve =>
+                        adm.extractAllToAsync(path.join(tmpDirForDownloadingPatch, "patch"), true, resolve));
+                    rimraf.sync(patchArchive);
+                    patchDir = path.resolve(tmpDirForDownloadingPatch, "patch/soda-player-plus-main/patch");
+                }
+
+                await fse.copy(
+                    patchDir,
+                    contentsDir,
+                    {
+                        overwrite: true,
+                    }
+                );
+                filePathRegexps(contentsDir, [
+                    {
+                        filePath: "index.html",
+                        replace: [
+                            [/<!-- Google Analytics -->.+?<!-- End Google Analytics -->/is, ""],
+                            [/<script src="vendor\/jquery\/dist\/jquery\.slim\.min\.js"/is,
+                                `<!-- PLUS SCRIPTS -->\n<script src="plus/renderer.js"></script>\n\n$&`]
+                        ]
+                    },
+                    {
+                        filePath: "package.json",
+                        replace: [
+                            [/js\/main\/main.js/i, "plus/main.js"]
+                        ]
+                    }
+                ]);
             }
-
-            await fse.copy(
-                patchDir,
-                contentsDir,
-                {
-                    overwrite: true,
-                }
-            );
-            filePathRegexps(contentsDir, [
-                {
-                    filePath: "index.html",
-                    replace: [
-                        [/<!-- Google Analytics -->.+?<!-- End Google Analytics -->/is, ""],
-                        [/<script src="vendor\/jquery\/dist\/jquery\.slim\.min\.js"/is,
-                            `<!-- PLUS SCRIPTS -->\n<script src="plus/renderer.js"></script>\n\n$&`]
-                    ]
-                },
-                {
-                    filePath: "package.json",
-                    replace: [
-                        [/js\/main\/main.js/i, "plus/main.js"]
-                    ]
-                }
-            ]);
-        }
-    });
-    if (!customPatchDirectory) {
-        rimraf.sync(
-            path.join(tmpDirForDownloadingPatch, "patch")
-        );
+        });
+    } finally {
+        const patchDir = path.join(tmpDirForDownloadingPatch, "patch");
+        if (fse.existsSync(patchDir)) rimraf.sync(patchDir);
     }
 };
